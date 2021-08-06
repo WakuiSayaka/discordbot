51a8368a0fb543cb884df2f703ae733e6bfbeaee
tsumugi <85925233+tsumugiw@users.noreply.github.com>
2021-08-07 06:21:44 +0900

    waitfor確認

--- discordbot.py
+++ discordbot.py
@@ -97,7 +97,7 @@ async def on_message(message):
             return user == message.author and str(reaction.emoji) == '👍'
 
         try:
-            reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
+            reaction, user = await client.wait_for('reaction_add', timeout=20.0, check=check)
         except asyncio.TimeoutError:
             await channel.send('👎')
         else:
