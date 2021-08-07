9b531ccd9e190e4fcfa51dc247003d60b1ca5d17
tsumugi <85925233+tsumugiw@users.noreply.github.com>
2021-08-07 16:27:41 +0900

    たいむあうと処理

--- discordbot.py
+++ discordbot.py
@@ -128,7 +128,7 @@ async def on_message(message):
 
     # じゃんけん
     if message.content == '/rsp':
-        # flag = 0
+        flag = 0
         rsp = ['ぐー','ちょき','ぱー']
         judge = ['引き分けです！','あなたの勝ちです！','わたしの勝ちです！']
         await message.channel.send(f'20秒以内に{rsp[0]}、{rsp[1]}、{rsp[2]}のどれかで返してね！')
@@ -144,13 +144,13 @@ async def on_message(message):
             await message.channel.send('なんでなん？')
         else:
             bot  = random.randint(0,2)
-            # flag = 1
+            flag = 1
             await message.channel.send(f'あなた：{player.content}')
             await message.channel.send(f'わたし：{rsp[bot]}')
             await message.channel.send(judge[(bot  - rsp.index(player.content) + 3)%3])
         finally:
-            # if flag == 0:
-            await message.channel.send('たいむあうと！')
+            if flag == 0:
+                await message.channel.send('たいむあうと！')
 
 
 
